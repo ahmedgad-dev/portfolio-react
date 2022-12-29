5cbc1158ef60a6d8f555de2a752c7fddc6eab863
ahmedGad-dev <76513984+ahmedGad-dev@users.noreply.github.com>
2022-12-29 16:45:16 +0200

    Final home page

--- src/pages/Github.js
+++ src/pages/Github.js
@@ -50,12 +50,12 @@ const Github = () => {
 
   return(
     (<section>
-       <h1>{githubUser.login}</h1>
-       <div className="page-intro flex flex-row align-center w-1/2 mt-16 mb-6">
-         <h2>Below are Statistics made by data coming from the github API</h2>
+      { githubUser && <h1 className='text-2xl sm:text-4xl md:text-6xl'>{githubUser.login}</h1>}
+       <div className="page-intro flex flex-row align-center mt-16 mb-6 justify-center align-center">
+         <h2 className='text-sm sm:text-xl lg:text-2xl italic justify-center w-2/3 md:w-1/2 md:justify-start'>Below are Statistics made by data coming from the github API</h2>
          <GithubLogo/>
        </div>
-      <Wrapper className='section-center'>     
+      <Wrapper className='items-center justify-center'>     
         <Pie3D data={mostUsed} />
         <Column3D data= {stars}/>   
         <Bar3D data={forks} />
